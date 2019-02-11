13f97aa95be6b53a4fdbbc384c02b129264adbbd
benAkehurst <benakehurst@hotmail.com>
2019-02-11 21:23:09 +0000

    Changed params on the booking model

--- server/api/models/bookingModel.js
+++ server/api/models/bookingModel.js
@@ -3,16 +3,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const BookingSchema = new Schema({
-  _bookingId: Schema.ObjectId,
   user: { type: Schema.ObjectId, ref: 'User'},
   bookingStart: Date,
   bookingEnd: Date,
   startHour: Number,
   duration: Number,
-  recurring: [],
-  floor: Number,
-  purpose: { type: String, required: true},
-  roomId: { type: Schema.ObjectId, ref: 'Room'}
+  purpose: { type: String, required: true}
 });
 
 
