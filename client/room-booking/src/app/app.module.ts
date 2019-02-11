d41dcb47ac5c9ede2bc16faff115bec9770afee2
benAkehurst <benakehurst@hotmail.com>
2019-02-11 22:44:19 +0000

    Adding Http Module to app module

--- client/room-booking/src/app/app.module.ts
+++ client/room-booking/src/app/app.module.ts
@@ -3,16 +3,20 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MaterialModule } from './material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     FormsModule,
     MaterialModule,
     BrowserAnimationsModule
