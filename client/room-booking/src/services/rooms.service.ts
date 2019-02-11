12650059c6b78c5993da3ddd6974110cabda7f88
benAkehurst <benakehurst@hotmail.com>
2019-02-11 22:44:35 +0000

    Get request on room service

--- client/room-booking/src/services/rooms.service.ts
+++ client/room-booking/src/services/rooms.service.ts
@@ -1,14 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RoomsService {
 
-  constructor() { }
+  private baseUrl: String = 'http://localhost:3000';
 
-/**
- * THIS WILL DEAL WITH TALKING TO THE API TO CALL ROOMS ACTIONS - FETCH ALL/FETCH SINGLE ROOM
- */
+  constructor(
+    private http: Http
+  ) { }
+
+  /**
+   * THIS WILL DEAL WITH TALKING TO THE API TO CALL ROOMS ACTIONS - FETCH ALL/FETCH SINGLE ROOM
+   */
+
+  public getAllRooms() {
+    return this.http.get(this.baseUrl + '/api/rooms').pipe(map((response: any) => response.json()));
+  }
 
 }
