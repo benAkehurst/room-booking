082cdf7fc35a242be4e1e599ab3966854b6d06c9
benAkehurst <benakehurst@hotmail.com>
2019-02-13 18:29:29 +0000

    Building logic for menu bar

--- client/room-booking/src/app/app.component.ts
+++ client/room-booking/src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from 'src/services/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   opened: Boolean;
+  showLoginLink: Boolean = false;
+  showRegisterLink: Boolean = true;
+
+  constructor(
+    private router: Router,
+    private user: UserService
+  ) { }
+
+  ngOnInit() {
+    this.checkUserState();
+  }
+
+  public checkUserState() {
+    const status = this.user.checkUserLoggedInStatus();
+    if (status === true) {
+      this.showLoginLink = false;
+      this.showRegisterLink = true;
+    }
+  }
+
+  public goHome() {
+    this.router.navigateByUrl('home');
+  }
+
+  public goToMyBookings() {
+    this.router.navigateByUrl('my-bookings');
+  }
+
+  public goToLogin() {
+    this.router.navigateByUrl('login');
+  }
+
+  public goToRegister() {
+    this.router.navigateByUrl('register');
+  }
 }
