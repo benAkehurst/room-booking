b04535b8218146fc82a47293cb7718fca00d55aa
benAkehurst <benakehurst@hotmail.com>
2019-02-16 11:25:44 +0000

    Added user functions to user service

--- client/room-booking/src/services/user.service.ts
+++ client/room-booking/src/services/user.service.ts
@@ -1,18 +1,32 @@
 import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+import UserDataModel from '../models/userDataModel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private headers = new Headers({ 'Content-Type': 'application/json' });
+  private baseUrl = 'http://localhost:3000/';
+  User: UserDataModel;
 
+  constructor(private http: Http) {
+    this.User = new UserDataModel();
+  }
+
+  /**
+   * Returns true if a user ID exits in local storage
+   */
   public checkUserLoggedInStatus() {
     if (this.getUserId()) {
       return true;
     }
   }
 
+  /**
+   * Checks local storage if a user ID exists in local storage
+   */
   public getUserId() {
     const userId = localStorage.getItem('id');
     if (userId != null ) {
