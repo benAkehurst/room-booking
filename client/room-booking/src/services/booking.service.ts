e1d7e16c66684b7f43ad2608770663ede618466b
benAkehurst <benakehurst@hotmail.com>
2019-02-16 11:26:21 +0000

    Added booking functions

--- client/room-booking/src/services/booking.service.ts
+++ client/room-booking/src/services/booking.service.ts
@@ -1,14 +1,40 @@
 import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+import UserDataModel from '../models/userDataModel';
+import BookingDataModel from '../models/bookingDataModel';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookingService {
 
-  constructor() { }
+  private headers = new Headers({ 'Content-Type': 'application/json' });
+  private baseUrl = 'http://localhost:3000/';
+  User: UserDataModel;
+  Booking: BookingDataModel;
 
-/**
- * THIS WILL DEAL WITH TALKING TO THE API TO CALL BOOKING ACTIONS - BOOKING/CANCELING A ROOM
- */
+  constructor(private http: Http) {
+    this.User = new UserDataModel();
+    this.Booking = new BookingDataModel();
+  }
+
+  /**
+   * This function sends a booking request to the server
+   */
+  public bookRoom(roomId) {
+    return this.http
+      .post(this.baseUrl + '/api/room/book/' + roomId, { data: this.Booking }, { headers: this.headers })
+      .pipe(map((response: any) => response.json()));
+  }
+
+  /**
+   * This function allows a user to cancel a booking
+   */
+  public cancelABooking(roomId) {
+    return this.http
+      .post(this.baseUrl + '/api/room/cancel/' + roomId, { data: this.Booking }, { headers: this.headers })
+      .pipe(map((response: any) => response.json()));
+  }
 
 }
