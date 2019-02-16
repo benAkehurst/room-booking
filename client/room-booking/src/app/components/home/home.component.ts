0edd15c44b5f610bf6cba55a1697b775416576a7
benAkehurst <benakehurst@hotmail.com>
2019-02-16 10:15:44 +0000

    Added Sweetalrt to home and starting room cards

--- client/room-booking/src/app/components/home/home.component.ts
+++ client/room-booking/src/app/components/home/home.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { RoomsService } from 'src/services/rooms.service';
 
+import Swal from 'sweetalert2';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+
+  allRooms: any = [];
 
   constructor(
     private rs: RoomsService
@@ -18,7 +22,28 @@ export class HomeComponent implements OnInit {
 
   public test() {
     this.rs.getAllRooms().subscribe((result) => {
-      console.log(result);
+      if (result.code === 204) {
+        this.openSwal(
+          'Error Receiving Rooms',
+          'There was a problem fetching the rooms',
+          'error',
+          'Ok...'
+        );
+      } else {
+        this.allRooms = result.data;
+      }
+      console.log(this.allRooms);
+    });
+  }
+
+  // Builds a basic sweet alert poput for error handling to user
+  public openSwal(title, text, warning, confirmText) {
+    Swal.fire({
+      title: title,
+      text: text,
+      type: warning,
+      showCancelButton: true,
+      confirmButtonText: confirmText
     });
   }
 
