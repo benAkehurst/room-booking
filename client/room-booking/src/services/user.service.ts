c6306cdd8f741fa819d7b4750c28c006c5f2d4e5
benAkehurst <benakehurst@hotmail.com>
2019-02-16 14:22:06 +0000

    Added crypto service for user id client side

--- client/room-booking/src/services/user.service.ts
+++ client/room-booking/src/services/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Headers, Http } from '@angular/http';
 import UserDataModel from '../models/userDataModel';
+import { CryptoService } from './crypto.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +11,33 @@ export class UserService {
   private headers = new Headers({ 'Content-Type': 'application/json' });
   private baseUrl = 'http://localhost:3000/';
   User: UserDataModel;
+  decryptedUserId: any;
 
-  constructor(private http: Http) {
+  constructor(
+    private http: Http,
+    private crypto: CryptoService
+  ) {
     this.User = new UserDataModel();
+  }
+
+  public encryptUserId(userId) {
+    this.crypto.encryptData(userId)
+      .then(
+        localStorage.setItem('id', userId)
+      )
+      .catch(
+        console.log('error')
+      );
+  }
+
+  public decryptUserId(userId) {
+    this.crypto.decryptData(userId)
+      .then(
+        this.decryptedUserId = userId
+      )
+      .catch(
+        console.log('error')
+      );
   }
 
   /**
@@ -24,13 +49,17 @@ export class UserService {
     }
   }
 
+  public checkIfUserAdmin() {
+
+  }
+
   /**
    * Checks local storage if a user ID exists in local storage
    */
   public getUserId() {
     const userId = localStorage.getItem('id');
-    if (userId != null ) {
-      return userId;
+    if (this.decryptUserId != null) {
+      return this.decryptUserId;
     }
   }
 }
